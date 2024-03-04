08a7f029948eba30c07a0d571d13533147df3ab2
scamgi <scampini.giacomo@gmail.com>
2024-03-04 21:01:08 +0100

    firebase installed

---
+++ src/config/firebase-config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBKn7kvBcJ6BnvBhgadHcd95QCqJJbz1v8",
+  authDomain: "tutorial-firebase-react-3e800.firebaseapp.com",
+  projectId: "tutorial-firebase-react-3e800",
+  storageBucket: "tutorial-firebase-react-3e800.appspot.com",
+  messagingSenderId: "16040314780",
+  appId: "1:16040314780:web:fcc719b539450c3c002a59",
+  measurementId: "G-LH7R2Q3RT8"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
