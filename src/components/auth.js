a0edbca2e1264423e838e7d36c5386f2f884777e
scamgi <scampini.giacomo@gmail.com>
2024-03-08 09:48:52 +0100

    indentation from 4 to 2 in auth.js

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,56 +1,57 @@
 import { useState } from "react";
 import { auth, googleProvider } from "../config/firebase-config";
-import { createUserWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 
 export const Auth = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    console.log(auth?.currentUser?.email);
+  console.log(auth?.currentUser?.email);
 
-    const signIn = async () => {
-        try {
-            await createUserWithEmailAndPassword(auth, email, password);
-        }
-        catch (err) {
-            console.error(err);
-        }
-    };
-
-    const signInWithGoogle = async () => {
-        try {
-            await signInWithPopup(auth, googleProvider);
-        }
-        catch (err) {
-            console.error(err);
-        }
-    };
-
-    const logout = async () => {
-        try {
-            await signOut(auth);
-        }
-        catch (err) {
-            console.error(err);
-        }
+  const signIn = async () => {
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+    } catch (err) {
+      console.error(err);
     }
+  };
 
-    return (
-        <div>
-            <input
-                placeholder="Email..."
-                onChange={(e) => setEmail(e.target.value)}
-            />
-            <input
-                placeholder="Password..."
-                type="password"
-                onChange={(e) => setPassword(e.target.value)}
-            />
-            <button onClick={signIn}>Sign in</button>
+  const signInWithGoogle = async () => {
+    try {
+      await signInWithPopup(auth, googleProvider);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
-            <button onClick={signInWithGoogle}>Sign in with google</button>
+  const logout = async () => {
+    try {
+      await signOut(auth);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
-            <button onClick={logout}>Logout</button>
-        </div>
-    );
-}
+  return (
+    <div>
+      <input
+        placeholder="Email..."
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        placeholder="Password..."
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={signIn}>Sign in</button>
+
+      <button onClick={signInWithGoogle}>Sign in with google</button>
+
+      <button onClick={logout}>Logout</button>
+    </div>
+  );
+};
